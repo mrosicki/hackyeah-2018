73b51fa09d0e7a308a91ea50314978ce9e6b8a7c
Michał Rotko <michalr835@gmail.com>
2018-11-24 20:34:46 +0100

    builder do db

--- app/src/main/java/com/prototype/hackyeah2018/MainActivity.java
+++ app/src/main/java/com/prototype/hackyeah2018/MainActivity.java
@@ -1,13 +1,19 @@
 package com.prototype.hackyeah2018;
 
+import com.prototype.hackyeah2018.db.AppDatabase;
+
+import android.arch.persistence.room.RoomDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = AppDatabase.getInstance(this);
     }
 }
--- app/src/main/java/com/prototype/hackyeah2018/db/AppDatabase.java
+++ app/src/main/java/com/prototype/hackyeah2018/db/AppDatabase.java
@@ -7,7 +7,9 @@ import com.prototype.hackyeah2018.model.Medicine;
 import com.prototype.hackyeah2018.model.Pharmacy;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = {
         Pharmacy.class,
@@ -16,9 +18,15 @@ import android.arch.persistence.room.RoomDatabase;
         version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
+    public static final String DATABASE_NAME = "APP";
+
     public abstract PharmacyDao getPharmacyDao();
 
     public abstract MedicineDao getMedicineDao();
 
     public abstract PharmacyWithMedicinesDao getPharmacyWithMedicinesDao();
+
+    public static AppDatabase getInstance(final Context context) {
+        return Room.databaseBuilder(context, AppDatabase.class, DATABASE_NAME).build();
+    }
 }
